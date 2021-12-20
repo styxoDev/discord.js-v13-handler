0283396c96817b538e30913b4b200b415a721f2c
Styxo <96294167+styxoDev@users.noreply.github.com>
2021-12-20 16:38:38 +0530

    Update example.js

--- src/Functions/events/interaction/example.js
+++ src/Functions/events/interaction/example.js
@@ -1,4 +1,9 @@
 /* Use this folder as to interact with Interaction ID'
-But don't to load in from loadEvents
+But don't forget to load in from loadEvents
 client.on('interactionCreate', (i) => interEvents('Name of the file')(i, client)) 
+
+Then here 
+module.exports = async(interaction, client) => {
+//Stuff
+}
 */
